64def1a8acfb56539b8188e3adf9a6e511689327
Mario Faúndez <mariofaundez@hotmail.com>
2016-11-01 23:25:20 -0300

    configuracion básica

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+from decouple import config
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cuantotiempo.settings")
+    settings_module = config('DJANGO_SETTINGS_MODULE', default=None)
+
+    if sys.argv[1] == 'test':
+        if settings_module:
+            print("Ignoring config('DJANGO_SETTINGS_MODULE') because it's test. "
+                  "Using '{{project_name}}.settings.test'")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.test")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
